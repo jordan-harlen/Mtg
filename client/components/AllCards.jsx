a5d5a4b5eed8f24321688e03da5de168ae7c022f
Jordan Harlen <jordan_harlen@hotmail.com>
2022-10-18 12:17:32 +1300

    split content into components and added page functionality to allCards

---
+++ client/components/AllCards.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react'
+
+import { getMtgApi } from '../apis/mtgGetApi'
+
+function Mtg() {
+  const [mtgArr, setMtgArr] = useState(null)
+  const [pageCounter, setPageCounter] = useState(1)
+
+  useEffect(() => {
+    getMtgApi(pageCounter)
+      .then((res) => {
+        setMtgArr(res.cards)
+        console.log(mtgArr)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }, [pageCounter])
+
+  async function nextPage() {
+    const page = (await pageCounter) + 1
+    setPageCounter(page)
+  }
+
+  async function previousPage() {
+    const page = (await pageCounter) - 1
+    setPageCounter(page)
+  }
+
+  return (
+    <div className="flex-child">
+      <button onClick={previousPage}>Previous</button>
+      <button onClick={nextPage}>Next</button>
+      <div className="card">
+        {mtgArr?.map((cards, idx) => {
+          return (
+            <div className="card-wapper" key={idx}>
+              <p>{cards.name}</p>
+              {cards.imageUrl ? (
+                <img src={cards.imageUrl} alt={cards.name} />
+              ) : (
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/en/thumb/a/aa/Magic_the_gathering-card_back.jpg/200px-Magic_the_gathering-card_back.jpg"
+                  alt="back of the card"
+                />
+              )}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Mtg
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,65 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-import { getMtgApi, getFindApi } from '../apis/mtgGetApi'
-
-function Mtg() {
-  const [mtgArr, setMtgObj] = useState(null)
-  const [mtgName, setName] = useState('')
-
-  const clickHander = () => {
-    return getMtgApi()
-      .then((obj) => setMtgObj(obj.cards))
-      .catch((err) => {
-        console.error(err.message)
-      })
-  }
-
-  const handleChange = (evt) => {
-    setName(evt.target.value)
-    console.log(evt.target.value)
-  }
-
-  const handelSubmit = (evt) => {
-    evt.preventDefault()
-    return getFindApi(mtgName)
-      .then((obj) => setMtgObj(obj.cards))
-      .catch((err) => {
-        console.error(err.message)
-      })
-  }
-
-  return (
-    <div className="flex-child">
-      <h1>MTG</h1>
-      <button onClick={clickHander}>Get cards</button>
-      <form onSubmit={handelSubmit}>
-        <div>
-          <label htmlFor="name" className="name-label">
-            Name:{' '}
-          </label>
-          <input type="text" id="name" name="name" onChange={handleChange} />
-          <button onClick={handelSubmit}>hit it</button>
-        </div>
-      </form>
-      <div className="card">
-        {mtgArr?.map((cards, idx) => {
-          return (
-            <div className="card-wapper" key={idx}>
-              <p>{cards.name}</p>
-              {cards.imageUrl ? (
-                <img src={cards.imageUrl} alt={cards.name} />
-              ) : (
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/thumb/a/aa/Magic_the_gathering-card_back.jpg/200px-Magic_the_gathering-card_back.jpg"
-                  alt="back of the card"
-                />
-              )}
-            </div>
-          )
-        })}
-      </div>
-    </div>
-  )
+function Home() {
+  return <h1>MTG</h1>
 }
 
-export default Mtg
+export default Home
