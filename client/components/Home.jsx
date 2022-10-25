1a1e7c01687e987ae14b6b0e6ce6c6fc7151ef8b
kristofferbacaling <kristofferbacaling@gmail.com>
2022-10-26 12:02:37 +1300

    added transtions on the home page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-//import { useSelector } from 'react-redux'
-//import { Link } from 'react-router-dom'
+import Fade from 'react-reveal/Fade'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { logoutUser } from '../actions/auth'
+
 function Home() {
+  const navigateTo = useNavigate()
+  const auth = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+
+  const logout = () => {
+    const confirmSuccess = () => navigateTo('/')
+    dispatch(logoutUser(confirmSuccess))
+  }
+
   return (
     <div className="home">
       <section className="home-one">
@@ -13,16 +26,39 @@ function Home() {
         </div>
       </section>
       <section className="home-two">
-        <div className="box-one">
-          <div className="box-content">
-            <h2 className="blocktext">build deck</h2>
+        <Fade left>
+          <div className="box-one">
+            <div className="box-content">
+              <h2 className="blocktext">build deck</h2>
+              <div className="box-nav-container">
+                <Link to={`/allcards/${auth?.user?.id}`}>All Cards</Link>
+                <Link to={`/search/${auth?.user?.id}`}>Search</Link>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="box-two">
-          <div className="box-content">
-            <h2 className="blocktext">view deck</h2>
+        </Fade>
+        <Fade right>
+          <div className="box-two">
+            <div className="box-content">
+              <h2 className="blocktext">view deck</h2>
+              <div className="box-nav-container">
+                {auth.isAuthenticated ? (
+                  <>
+                    <Link to={`/userdeck/${auth?.user?.id}`}>My Deck</Link>
+                    <Link to="/" onClick={logout} className="title-font">
+                      Logout
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                  </>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
+        </Fade>
       </section>
     </div>
   )
